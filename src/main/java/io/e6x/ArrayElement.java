187271edee1abc042d3237dcc78a5d39f81a1d4c
priyanshu <priyanshu@e6x.o>
2023-07-27 17:12:29 +0530

    external merge try 1. need to resolve the todos

--- src/main/java/io/e6x/ArrayElement.java
+++ src/main/java/io/e6x/ArrayElement.java
@@ -2,12 +2,11 @@ package io.e6x;
 
 class ArrayElement implements Comparable<ArrayElement> {
     int value;
-    int elemIndex;
+
     int chunkNumber;
 
     public ArrayElement(int value, int chunkNumber) {
         this.value = value;
-        this.elemIndex = elemIndex;
         this.chunkNumber = chunkNumber;
     }
 
--- src/main/java/io/e6x/SortAndStore.java
+++ src/main/java/io/e6x/SortAndStore.java
@@ -8,8 +8,8 @@ public class SortAndStore {
 
     public static void main(String[] args) {
         final long len = 2L * 1024 * 1024 * 1024 / 4;
-
-
+        long section_len = 2L * 1024 * 1024 * 1024;
+        //TODO : put for loop inside try block
         for (int i = 0; i < 25; i++) {
             int[] inputData = new int[(int) len];
             long offset = i * 2 * 1024 * 1024 *1024;
@@ -17,11 +17,11 @@ public class SortAndStore {
             ) {
 
                 is.skipBytes((int) offset);
-                int i=0;
+                int j=0;
                 try {
                     while (true) {
                         inputData[i] = is.readInt();
-                        i += 1;
+                        j += 1;
                     }
                 } catch (EOFException e) {
 
@@ -43,53 +43,61 @@ public class SortAndStore {
                 e.printStackTrace();
             }
         }
-        mergeArraysWithHeaps();
-
-        //merge the files stored in hard disk
-
-
+        mergeArraysWithHeaps((int)section_len);
 
     }
 
 
 
-    private static void mergeArraysWithHeaps(int[] inputData, int cores, int section_len, long len) {
+    private static void mergeArraysWithHeaps(int section_len) {
 
         long startOfMergeTime = System.currentTimeMillis();
 
         PriorityQueue<ArrayElement> pq = new PriorityQueue<>();
-        int[] sortedData = new int[(int) len];
-        int[] sectionIndex = new int[25];
 
+        int[] sectionIndex = new int[25];
+        //TODO : dump sorted chunks into diff files
         for (int i = 0; i < 25; i++) {
             int offset = i * 2 * 1024 * 1024 *1024;
             sectionIndex[i] = offset;
         }
 
         try(RandomAccessFile randomAccessFile = new RandomAccessFile("/home/priyanshu/Desktop/asgnone/inputFile", "r")){
+            for (int i=0;i<25;i++) {
+                randomAccessFile.seek(sectionIndex[i]);
+                DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(randomAccessFile.getFD())));
+                int val =  dataInputStream.readInt();
+                pq.offer(new ArrayElement(val, i));
+
+            }
+
+            try(DataOutputStream os = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("/home/priyanshu/Desktop/asgnone/outputFile")));
+                DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(new FileInputStream(randomAccessFile.getFD())));){
+                while(!pq.isEmpty()) {
+                    ArrayElement minElement = pq.poll();
+                    int minVal = minElement.getValue();
+                    int chunkNumber = minElement.getChunkNumber();
+
+                    os.writeInt(minVal);
+                    sectionIndex[chunkNumber]+=32;
+
+                    if(sectionIndex[chunkNumber] < (chunkNumber + 1)*section_len){
+                        randomAccessFile.seek(sectionIndex[chunkNumber]);
+                        int val = dataInputStream.readInt();
+                        pq.offer(new ArrayElement(val, chunkNumber));
+
+                    }
+
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
 
         }catch(Exception e){
             e.printStackTrace();
         }
 
-        int i = 0;
-        for (int j = 0; j < len - cores; j++){
-            ArrayElement minElement = pq.poll();
-            int minVal = minElement.getValue();
-            int chunkNumber = minElement.getChunkNumber();
-
-            inputData[i] = minVal;
-            sectionIndex[chunkNumber]++;
-
-            if (sectionIndex[chunkNumber] < (chunkNumber + 1) * section_len) {
-                pq.offer(new ArrayElement(inputData[sectionIndex[chunkNumber]],  chunkNumber));
-            }
-            i++;
-
-        }
-        long elapsedTime = (System.currentTimeMillis() - startOfMergeTime);
-        System.out.println("Time for merge function: " + elapsedTime + " ms");
-        System.arraycopy(sortedData, 0, inputData, 0, (int) len);
     }
 }
 
