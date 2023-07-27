d07305421b08d729b351c6dfa629a704fcdcf0fc
priyanshu <priyanshu@e6x.o>
2023-07-27 14:33:35 +0530

    external merge

--- src/main/java/io/e6x/SortAndStore.java
+++ src/main/java/io/e6x/SortAndStore.java
@@ -1,125 +1,56 @@
 package io.e6x;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class SortAndStore {
 
     public static void main(String[] args) {
         final long len = 2L * 1024 * 1024 * 1024 / 4;
-        int[] inputData = new int[(int) len];
 
 
-        int numsRead = 0;
-        try (DataInputStream is = new DataInputStream(new BufferedInputStream(new FileInputStream("/home/priyanshu/Desktop/asgnone/inputFile")))
-        ) {
+        for (int i = 0; i < 25; i++) {
+            int[] inputData = new int[(int) len];
+            long offset = i * 2 * 1024 * 1024 *1024;
+            try (DataInputStream is = new DataInputStream(new BufferedInputStream(new FileInputStream("/home/priyanshu/Desktop/asgnone/inputFile")))
+            ) {
 
-            //takes input and stores in list
+                is.skipBytes((int) offset);
+                int i=0;
+                try {
+                    while (true) {
+                        inputData[i] = is.readInt();
+                        i += 1;
+                    }
+                } catch (EOFException e) {
 
-            int i=0;
-            try {
-                while (true) {
-                    inputData[i] = is.readInt();
-                    i += 1;
                 }
-            } catch (EOFException e) {
-                // End-of-file reached, stop reading
-            }
 
-            System.out.println("Integers read: " + i);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //using executor service class to solve the sorting using concurrency
-
-        int cores = Runtime.getRuntime().availableProcessors();
-        int lengthToSort = (int) (len / cores);
-
-        long startTime = System.currentTimeMillis();
-
-        ExecutorService executorService = Executors.newFixedThreadPool(cores);
-        List<Future<?>> allFuture = new ArrayList<>();
-
-        for (int i = 0; i < cores; i++) {
-            int startIdx = i * lengthToSort;
-            int endIdx = startIdx + lengthToSort;
-            if (i == cores - 1) {
-
-                endIdx = (int) len;
-            }
-            allFuture.add(executorService.submit(new Sort(inputData, startIdx, endIdx)));
-        }
-
-        for (Future<?> future : allFuture) {
-            try {
-                future.get();
-            } catch (InterruptedException | ExecutionException e) {
+                System.out.println("Integers read: " + i);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+            Arrays.sort(inputData);
 
-        long endTime = System.currentTimeMillis();
-        long executionTime = endTime - startTime;
-        System.out.println("Execution Time for sorting parallel: " + executionTime + " milliseconds");
-        System.out.println("Memory used: " + (Runtime.getRuntime().totalMemory() / (1024 * 1024)) + " MB");
-
-        //merge the sorted section
-        long startTime2 = System.currentTimeMillis();
-
-        int sectionSize = (int) len / cores;
-
-         //using merging of parts by iterating
-
-        /*for (int i = 1; i < cores; i++) {
-            mergeSortedSections(inputData, 0, i * sectionSize - 1, (i+1) * sectionSize  - 1);
-        }*/
-
-
-        //merging using min heap
-
-
-        mergeArraysWithHeaps(inputData, cores, sectionSize, len);
-
-
-        System.out.println("Memory used: " + (Runtime.getRuntime().totalMemory() / (1024 * 1024)) + " MB");
-
-        long endTime2 = System.currentTimeMillis();
-        long executionTime2 = endTime2 - startTime2;
-        System.out.println("Execution Time for merging: " + executionTime2 + " milliseconds");
-
-        //final check
-        int flag = 0;
-        for (int i = 1; i < len; i++) {
-            if (inputData[i] < inputData[i - 1]) {
-                flag = 1;
-                break;
-            }
-        }
-
-        System.out.println("Memory used: " + (Runtime.getRuntime().totalMemory() / (1024 * 1024)) + " MB");
-
-        if (flag == 0) {
-            System.out.println("data is sorted");
-
-            //store in file output
-            try (BufferedWriter os = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/priyanshu/Desktop/asgnone/outputFile")))) {
+            try (   FileOutputStream fileOutputStream = new FileOutputStream("/home/priyanshu/Desktop/asgnone/inputFile");
+                    DataOutputStream os = new DataOutputStream(new BufferedOutputStream(fileOutputStream))) {
+                fileOutputStream.getChannel().position((int) offset);
                 for (int number : inputData) {
-                    os.write(number);
+                    os.writeInt(number);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            System.out.println("Output file created for sorted data.");
-        } else {
-            System.out.println("data is not sorted");
         }
+        mergeArraysWithHeaps();
+
+        //merge the files stored in hard disk
+
+
+
     }
+
 
 
     private static void mergeArraysWithHeaps(int[] inputData, int cores, int section_len, long len) {
@@ -128,14 +59,17 @@ public class SortAndStore {
 
         PriorityQueue<ArrayElement> pq = new PriorityQueue<>();
         int[] sortedData = new int[(int) len];
-        int[] sectionIndex = new int[cores];
+        int[] sectionIndex = new int[25];
 
-        for (int chunkIdx = 0; chunkIdx < cores; chunkIdx++){
-            pq.offer(new ArrayElement(inputData[chunkIdx * section_len],  chunkIdx));
-
+        for (int i = 0; i < 25; i++) {
+            int offset = i * 2 * 1024 * 1024 *1024;
+            sectionIndex[i] = offset;
         }
-        for (int chunkIdx = 0; chunkIdx < cores; chunkIdx++){
-            sectionIndex[chunkIdx] = chunkIdx * section_len;
+
+        try(RandomAccessFile randomAccessFile = new RandomAccessFile("/home/priyanshu/Desktop/asgnone/inputFile", "r")){
+
+        }catch(Exception e){
+            e.printStackTrace();
         }
 
         int i = 0;
